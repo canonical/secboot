898ad5ea6ffcf3218165c092bc0c605cc9a0dded
Chris Coulson <chris.coulson@canonical.com>
2021-03-22 10:29:14 +0000

    luks2.Activate: capture stdout and stderr from systemd-cryptstup and return these in the error if it fails

--- internal/luks2/activate.go
+++ internal/luks2/activate.go
@@ -20,14 +20,11 @@
 package luks2
 
 import (
-	"bufio"
 	"bytes"
-	"fmt"
 	"os"
 	"os/exec"
-	"sync"
 
-	"golang.org/x/xerrors"
+	"github.com/snapcore/snapd/osutil"
 )
 
 var (
@@ -36,45 +33,15 @@ var (
 
 // Activate unlocks the LUKS device at sourceDevicePath using systemd-cryptsetup and creates a device
 // mapping with the supplied volumeName. The device is unlocked using the supplied key.
-//
-// This will return a *os.ExitError error in the event that systemd-cryptsetup fails.
 func Activate(volumeName, sourceDevicePath string, key []byte) error {
 	cmd := exec.Command(systemdCryptsetupPath, "attach", volumeName, sourceDevicePath, "/dev/stdin", "luks,tries=1")
 	cmd.Env = os.Environ()
 	cmd.Env = append(cmd.Env, "SYSTEMD_LOG_TARGET=console")
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		return xerrors.Errorf("cannot create stdout pipe: %w", err)
-	}
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		return xerrors.Errorf("cannot create stderr pipe: %w", err)
-	}
-
 	cmd.Stdin = bytes.NewReader(key)
 
-	if err := cmd.Start(); err != nil {
-		return err
+	if output, err := cmd.CombinedOutput(); err != nil {
+		return osutil.OutputErr(output, err)
 	}
 
-	var wg sync.WaitGroup
-	wg.Add(2)
-	go func() {
-		rd := bufio.NewScanner(stdout)
-		for rd.Scan() {
-			fmt.Printf("systemd-cryptsetup: %s\n", rd.Text())
-		}
-		wg.Done()
-	}()
-	go func() {
-		rd := bufio.NewScanner(stderr)
-		for rd.Scan() {
-			fmt.Fprintf(os.Stderr, "systemd-cryptsetup: %s\n", rd.Text())
-		}
-		wg.Done()
-	}()
-
-	wg.Wait()
-
-	return cmd.Wait()
+	return nil
 }
