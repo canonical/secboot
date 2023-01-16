70f092c7e232001579c2f6671478cd22a5afb425
Chris Coulson <chris.coulson@canonical.com>
2023-01-16 14:35:21 +0000

    internal/testutil: Add MockKDF
    This moves the mock KDF implementation in secboot_test to
    internal/testutil, as it's going to be used inside the tpm2 package in a
    follow up PR.

--- argon2_test.go
+++ argon2_test.go
@@ -20,14 +20,11 @@
 package secboot_test
 
 import (
-	"crypto"
-	"encoding/binary"
 	"math"
 	"os"
 	"runtime"
 	"time"
 
-	"github.com/canonical/go-sp800.108-kdf"
 	snapd_testutil "github.com/snapcore/snapd/testutil"
 
 	"golang.org/x/sys/unix"
@@ -38,28 +35,6 @@ import (
 	"github.com/snapcore/secboot/internal/argon2"
 	"github.com/snapcore/secboot/internal/testutil"
 )
-
-type mockKDF struct {
-	lastBenchmarkKeyLen uint32
-}
-
-func (_ *mockKDF) Derive(passphrase string, salt []byte, params *KDFCostParams, keyLen uint32) ([]byte, error) {
-	context := make([]byte, len(salt)+9)
-	copy(context, salt)
-	binary.LittleEndian.PutUint32(context[len(salt):], params.Time)
-	binary.LittleEndian.PutUint32(context[len(salt)+4:], params.MemoryKiB)
-	context[len(salt)+8] = params.Threads
-
-	return kdf.CounterModeKey(kdf.NewHMACPRF(crypto.SHA256), []byte(passphrase), nil, context, keyLen*8), nil
-}
-
-func (k *mockKDF) Time(params *KDFCostParams, keyLen uint32) (time.Duration, error) {
-	k.lastBenchmarkKeyLen = keyLen
-
-	const memBandwidthKiBPerMs = 2048
-	duration := (time.Duration(float64(params.MemoryKiB)/float64(memBandwidthKiBPerMs)) * time.Duration(params.Time)) * time.Millisecond
-	return duration, nil
-}
 
 type argon2Suite struct {
 	halfTotalRamKiB uint64
@@ -92,7 +67,7 @@ func (s *argon2Suite) checkParams(c *C, opts *KDFOptions, ncpus int, params *KDF
 		if targetDuration == 0 {
 			targetDuration = 2 * time.Second
 		}
-		var kdf mockKDF
+		var kdf testutil.MockKDF
 		duration, _ := kdf.Time(params, 0)
 		c.Check(duration, Equals, targetDuration)
 
@@ -119,47 +94,47 @@ func (s *argon2Suite) checkParams(c *C, opts *KDFOptions, ncpus int, params *KDF
 var _ = Suite(&argon2Suite{})
 
 func (s *argon2Suite) TestDeriveCostParamsDefault(c *C) {
-	var kdf mockKDF
+	var kdf testutil.MockKDF
 
 	var opts KDFOptions
 	params, err := opts.DeriveCostParams(48, &kdf)
 	c.Assert(err, IsNil)
-	c.Check(kdf.lastBenchmarkKeyLen, Equals, uint32(48))
+	c.Check(kdf.BenchmarkKeyLen, Equals, uint32(48))
 
 	s.checkParams(c, &opts, s.cpus, params)
 }
 
 func (s *argon2Suite) TestDeriveCostParamsMemoryLimit(c *C) {
-	var kdf mockKDF
+	var kdf testutil.MockKDF
 
 	var opts KDFOptions
 	opts.MemoryKiB = 32 * 1024
 	params, err := opts.DeriveCostParams(48, &kdf)
 	c.Assert(err, IsNil)
-	c.Check(kdf.lastBenchmarkKeyLen, Equals, uint32(48))
+	c.Check(kdf.BenchmarkKeyLen, Equals, uint32(48))
 
 	s.checkParams(c, &opts, s.cpus, params)
 }
 
 func (s *argon2Suite) TestDeriveCostParamsForceBenchmarkedThreads(c *C) {
-	var kdf mockKDF
+	var kdf testutil.MockKDF
 
 	var opts KDFOptions
 	opts.Parallel = 1
 	params, err := opts.DeriveCostParams(48, &kdf)
 	c.Assert(err, IsNil)
-	c.Check(kdf.lastBenchmarkKeyLen, Equals, uint32(48))
+	c.Check(kdf.BenchmarkKeyLen, Equals, uint32(48))
 
 	s.checkParams(c, &opts, s.cpus, params)
 }
 
 func (s *argon2Suite) TestDeriveCostParamsDifferentKeyLen(c *C) {
-	var kdf mockKDF
+	var kdf testutil.MockKDF
 
 	var opts KDFOptions
 	params, err := opts.DeriveCostParams(32, &kdf)
 	c.Assert(err, IsNil)
-	c.Check(kdf.lastBenchmarkKeyLen, Equals, uint32(32))
+	c.Check(kdf.BenchmarkKeyLen, Equals, uint32(32))
 
 	s.checkParams(c, &opts, s.cpus, params)
 }
@@ -168,13 +143,13 @@ func (s *argon2Suite) TestDeriveCostParamsForceIterations(c *C) {
 	restore := MockRuntimeNumCPU(2)
 	defer restore()
 
-	var kdf mockKDF
+	var kdf testutil.MockKDF
 
 	var opts KDFOptions
 	opts.ForceIterations = 3
 	params, err := opts.DeriveCostParams(48, &kdf)
 	c.Assert(err, IsNil)
-	c.Check(kdf.lastBenchmarkKeyLen, Equals, uint32(0))
+	c.Check(kdf.BenchmarkKeyLen, Equals, uint32(0))
 
 	s.checkParams(c, &opts, 2, params)
 }
@@ -183,14 +158,14 @@ func (s *argon2Suite) TestDeriveCostParamsForceMemory(c *C) {
 	restore := MockRuntimeNumCPU(2)
 	defer restore()
 
-	var kdf mockKDF
+	var kdf testutil.MockKDF
 
 	var opts KDFOptions
 	opts.ForceIterations = 3
 	opts.MemoryKiB = 32 * 1024
 	params, err := opts.DeriveCostParams(48, &kdf)
 	c.Assert(err, IsNil)
-	c.Check(kdf.lastBenchmarkKeyLen, Equals, uint32(0))
+	c.Check(kdf.BenchmarkKeyLen, Equals, uint32(0))
 
 	s.checkParams(c, &opts, 2, params)
 }
@@ -199,13 +174,13 @@ func (s *argon2Suite) TestDeriveCostParamsForceIterationsDifferentCPUNum(c *C) {
 	restore := MockRuntimeNumCPU(8)
 	defer restore()
 
-	var kdf mockKDF
+	var kdf testutil.MockKDF
 
 	var opts KDFOptions
 	opts.ForceIterations = 3
 	params, err := opts.DeriveCostParams(48, &kdf)
 	c.Assert(err, IsNil)
-	c.Check(kdf.lastBenchmarkKeyLen, Equals, uint32(0))
+	c.Check(kdf.BenchmarkKeyLen, Equals, uint32(0))
 
 	s.checkParams(c, &opts, 4, params)
 }
@@ -214,14 +189,14 @@ func (s *argon2Suite) TestDeriveCostParamsForceThreads(c *C) {
 	restore := MockRuntimeNumCPU(8)
 	defer restore()
 
-	var kdf mockKDF
+	var kdf testutil.MockKDF
 
 	var opts KDFOptions
 	opts.ForceIterations = 3
 	opts.Parallel = 1
 	params, err := opts.DeriveCostParams(48, &kdf)
 	c.Assert(err, IsNil)
-	c.Check(kdf.lastBenchmarkKeyLen, Equals, uint32(0))
+	c.Check(kdf.BenchmarkKeyLen, Equals, uint32(0))
 
 	s.checkParams(c, &opts, 1, params)
 }
