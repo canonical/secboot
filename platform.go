74f8b089d610f8b11f388f08c1f7daad4bd8839d
Spyros Seimenis <spyros.seimenis@canonical.com>
2023-10-16 14:30:08 +0200

    platform.go: add role field to PlatformKeyData
    Under the new design, the platform agnostic API provides a new
    role field so each key's use can be identified by snapd.
    
    This role field will be authenticated "indirectly" for the case of
    v3 keys by binding it to the authorization policy of the sealed
    object.

--- platform.go
+++ platform.go
@@ -68,6 +68,7 @@ func (e *PlatformHandlerError) Unwrap() error {
 type PlatformKeyData struct {
 	Generation    int
 	EncodedHandle []byte // The JSON encoded platform handle
+	Role          string
 	KDFAlg        crypto.Hash
 
 	AuthMode AuthMode
