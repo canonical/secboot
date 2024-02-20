6d262fe7a4330ecdd20d0c1f2084a0a433063f02
Spyros Seimenis <spyros.seimenis@canonical.com>
2024-02-20 13:59:03 +0200

    keydata_legacy_test.go: merge keyDataLegacyTestBase in keyDataLegacySuite

--- keydata_legacy_test.go
+++ keydata_legacy_test.go
@@ -25,26 +25,28 @@ func marshalV1Keys(key DiskUnlockKey, auxKey PrimaryKey) []byte {
 	return w.Bytes()
 }
 
-type keyDataLegacyTestBase struct {
+type keyDataLegacySuite struct {
 	keyDataTestBase
 }
 
-func (s *keyDataLegacyTestBase) SetUpSuite(c *C) {
+var _ = Suite(&keyDataLegacySuite{})
+
+func (s *keyDataLegacySuite) SetUpSuite(c *C) {
 	s.handler = &mockPlatformKeyDataHandler{}
 	s.mockPlatformName = "mock-legacy"
 	RegisterPlatformKeyDataHandler(s.mockPlatformName, s.handler)
 }
 
-func (s *keyDataLegacyTestBase) SetUpTest(c *C) {
+func (s *keyDataLegacySuite) SetUpTest(c *C) {
 	s.handler.state = mockPlatformDeviceStateOK
 	s.handler.passphraseSupport = false
 }
 
-func (s *keyDataLegacyTestBase) TearDownSuite(c *C) {
+func (s *keyDataLegacySuite) TearDownSuite(c *C) {
 	RegisterPlatformKeyDataHandler(s.mockPlatformName, nil)
 }
 
-func (s *keyDataLegacyTestBase) newKeyDataKeys(c *C, sz1, sz2 int) (DiskUnlockKey, PrimaryKey) {
+func (s *keyDataLegacySuite) newKeyDataKeys(c *C, sz1, sz2 int) (DiskUnlockKey, PrimaryKey) {
 	key := make([]byte, sz1)
 	auxKey := make([]byte, sz2)
 	_, err := rand.Read(key)
@@ -54,7 +56,7 @@ func (s *keyDataLegacyTestBase) newKeyDataKeys(c *C, sz1, sz2 int) (DiskUnlockKe
 	return key, auxKey
 }
 
-func (s *keyDataLegacyTestBase) mockProtectKeys(c *C, key DiskUnlockKey, auxKey PrimaryKey, kdfAlg crypto.Hash, modelAuthHash crypto.Hash) (out *KeyParams) {
+func (s *keyDataLegacySuite) mockProtectKeys(c *C, key DiskUnlockKey, auxKey PrimaryKey, kdfAlg crypto.Hash, modelAuthHash crypto.Hash) (out *KeyParams) {
 	payload := marshalV1Keys(key, auxKey)
 
 	k := make([]byte, 48)
@@ -83,12 +85,6 @@ func (s *keyDataLegacyTestBase) mockProtectKeys(c *C, key DiskUnlockKey, auxKey 
 	stream.XORKeyStream(out.EncryptedPayload, payload)
 	return
 }
-
-type keyDataLegacySuite struct {
-	keyDataLegacyTestBase
-}
-
-var _ = Suite(&keyDataLegacySuite{})
 
 type testLegacyKeyPayloadData struct {
 	key    DiskUnlockKey
