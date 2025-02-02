a161e36a8121e86ded67aaf7452967071da09c5f
Chris Coulson <chris.coulson@canonical.com>
2025-02-02 23:28:06 +0100

    efi/preinstall: Fail by default if PCR0 is unusable, we have a dTPM and the startup locality is 0
    if we have a discrete TPM (dTPM) and the TPM2_Startup command was executed from
    locality 3, or there was a H-CRTM event, we return an error by default if the
    corresponding localities 3 or 4 are not locked down by the silicon vendor's
    firmware before handing over to the platform firmware (we assume Intel platforms
    with TXT do lock these down because TXT provides the primitives to create a
    DRTM), returning a ErrTPMStartupLocalityNotProtected error to indicate that we
    can't enable partial mitigation of PCR replay from software in the event of the
    dTPM being reset independently of the host platform (either because a platform
    integration bug which makes this trivially possible with a pair of tweezers or
    because an adversary is determined enough to perform the necessary hardware
    tampering to trigger a reset of the dTPM independently of the host platform),
    after which they are able execute commands (eg, TPM2_Startup) at their desired
    locality from ring 0 in order to replay arbitrary PCR values from software and
    spoof any host platform.
    
    We provide a flag (PermitNoDiscreteTPMResetMitigation) to make this check
    non-fatal if desired. In this case, the returned result will have the
    StartupLocalityNotProtected flag to tell WithAutoTCGPCRProfile not to include
    PCR0. The next paragraph describes the relevance of PCR0.
    
    Our partial mitigation is achieved by including PCR0 in the policy if
    TPM2_Startup was not executed from locality 0, because it changes the initial
    value of the PCR. If TPM2_Startup occurred from locality 3 or there was a
    H-CRTM event, and access to these localities is subsequently restricted by the
    silicon vendor's firmware before handing over to the platform manufacturer's
    firmware, it's impossible to replay the PCR0 values from software in the event
    of a dTPM being reset independently of the host platform, even from platform
    firmware code or other ring 0 code. Of course, a more determined adversary can
    still spoof a host platform by executing commands at any locality if they are
    able to actively communicate directly with the dTPM's bus, which is why it is
    only a partial mitigation, and which is why there is an option to opt out of it.
    
    Previously, the checks were always succeeding but adding the
    StartupLocalityNotProtected flag to the returned results if TPM2_Startup was
    executed from locality 0. This is clearly a case where the partial mitigation
    cannot be enabled, as the initial value of PCR0 in this case is 0, making it
    trivial to replay from software in the event of the dTPM being reset
    independently of the host platform. This behaviour is wrong though. If a dTPM
    is detected and TPM2_Startup occurred from locality 0, the default behaviour
    should be to return an error (ErrTPMStartupLocalityNotProtected), with the
    option to use the existing flag (PermitNoDiscreteTPMResetMitigation) to make
    this non-fatal.

--- efi/preinstall/checks_test.go
+++ efi/preinstall/checks_test.go
@@ -3199,6 +3199,77 @@ C7E003CB
 	c.Check(errors.Is(sbe, ErrNoDeployedMode), testutil.IsTrue)
 }
 
+func (s *runChecksSuite) TestRunChecksBadDiscreteTPMDetectedSL0NotProtected(c *C) {
+	meiAttrs := map[string][]byte{
+		"fw_ver": []byte(`0:16.1.27.2176
+0:16.1.27.2176
+0:16.0.15.1624
+`),
+		"fw_status": []byte(`94000245
+09F10506
+00000020
+00004000
+00041F03
+C7E003CB
+`),
+	}
+	devices := map[string][]internal_efi.SysfsDevice{
+		"iommu": []internal_efi.SysfsDevice{
+			efitest.NewMockSysfsDevice("dmar0", "/sys/devices/virtual/iommu/dmar0", "iommu", nil),
+			efitest.NewMockSysfsDevice("dmar1", "/sys/devices/virtual/iommu/dmar1", "iommu", nil),
+		},
+		"mei": []internal_efi.SysfsDevice{
+			efitest.NewMockSysfsDevice("mei0", "/sys/devices/pci0000:00/0000:00:16.0/mei/mei0", "mei", meiAttrs),
+		},
+	}
+
+	_, err := s.testRunChecks(c, &testRunChecksParams{
+		env: efitest.NewMockHostEnvironmentWithOpts(
+			efitest.WithVirtMode(internal_efi.VirtModeNone, internal_efi.DetectVirtModeAll),
+			efitest.WithTPMDevice(tpm2_testutil.NewTransportBackedDevice(s.Transport, false, 1)),
+			efitest.WithLog(efitest.NewLog(c, &efitest.LogOptions{Algorithms: []tpm2.HashAlgorithmId{tpm2.HashAlgorithmSHA256}})),
+			efitest.WithAMD64Environment("GenuineIntel", []uint64{cpuid.SDBG}, 4, map[uint32]uint64{0xc80: 0x40000000}),
+			efitest.WithSysfsDevices(devices),
+			efitest.WithMockVars(efitest.MockVars{
+				{Name: "AuditMode", GUID: efi.GlobalVariable}:              &efitest.VarEntry{Attrs: efi.AttributeNonVolatile | efi.AttributeBootserviceAccess | efi.AttributeRuntimeAccess, Payload: []byte{0x0}},
+				{Name: "BootCurrent", GUID: efi.GlobalVariable}:            &efitest.VarEntry{Attrs: efi.AttributeBootserviceAccess | efi.AttributeRuntimeAccess, Payload: []byte{0x3, 0x0}},
+				{Name: "BootOptionSupport", GUID: efi.GlobalVariable}:      &efitest.VarEntry{Attrs: efi.AttributeBootserviceAccess | efi.AttributeRuntimeAccess, Payload: []byte{0x13, 0x03, 0x00, 0x00}},
+				{Name: "DeployedMode", GUID: efi.GlobalVariable}:           &efitest.VarEntry{Attrs: efi.AttributeNonVolatile | efi.AttributeBootserviceAccess | efi.AttributeRuntimeAccess, Payload: []byte{0x1}},
+				{Name: "SetupMode", GUID: efi.GlobalVariable}:              &efitest.VarEntry{Attrs: efi.AttributeBootserviceAccess | efi.AttributeRuntimeAccess, Payload: []byte{0x0}},
+				{Name: "OsIndicationsSupported", GUID: efi.GlobalVariable}: &efitest.VarEntry{Attrs: efi.AttributeBootserviceAccess | efi.AttributeRuntimeAccess, Payload: []byte{0x41, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}},
+			}.SetSecureBoot(true).SetPK(c, efitest.NewSignatureListX509(c, snakeoilCert, efi.MakeGUID(0x03f66fa4, 0x5eee, 0x479c, 0xa408, [...]uint8{0xc4, 0xdc, 0x0a, 0x33, 0xfc, 0xde})))),
+		),
+		tpmPropertyModifiers: map[tpm2.Property]uint32{
+			tpm2.PropertyNVCountersMax:     0,
+			tpm2.PropertyPSFamilyIndicator: 1,
+			tpm2.PropertyManufacturer:      uint32(tpm2.TPMManufacturerNTC),
+		},
+		enabledBanks: []tpm2.HashAlgorithmId{tpm2.HashAlgorithmSHA256},
+		loadedImages: []secboot_efi.Image{
+			&mockImage{
+				contents: []byte("mock shim executable"),
+				digest:   testutil.DecodeHexString(c, "25e1b08db2f31ff5f5d2ea53e1a1e8fda6e1d81af4f26a7908071f1dec8611b7"),
+				signatures: []*efi.WinCertificateAuthenticode{
+					efitest.ReadWinCertificateAuthenticodeDetached(c, shimUbuntuSig4),
+				},
+			},
+			&mockImage{contents: []byte("mock grub executable"), digest: testutil.DecodeHexString(c, "d5a9780e9f6a43c2e53fe9fda547be77f7783f31aea8013783242b040ff21dc0")},
+			&mockImage{contents: []byte("mock kernel executable"), digest: testutil.DecodeHexString(c, "2ddfbd91fa1698b0d133c38ba90dbba76c9e08371ff83d03b5fb4c2e56d7e81f")},
+		},
+		expectedPcrAlg: tpm2.HashAlgorithmSHA256,
+	})
+	c.Check(err, ErrorMatches, `one or more errors detected:
+- access to the discrete TPM's startup locality is available to platform firmware and privileged OS code, preventing any mitigation against reset attacks
+`)
+
+	var rce *RunChecksErrors
+	c.Assert(errors.As(err, &rce), testutil.IsTrue)
+	c.Assert(rce.Errs, HasLen, 1)
+
+	err = rce.Errs[0]
+	c.Check(errors.Is(err, ErrTPMStartupLocalityNotProtected), testutil.IsTrue)
+}
+
 func (s *runChecksSuite) TestRunChecksBadDiscreteTPMDetectedSL3NotProtected(c *C) {
 	meiAttrs := map[string][]byte{
 		"fw_ver": []byte(`0:16.1.27.2176
